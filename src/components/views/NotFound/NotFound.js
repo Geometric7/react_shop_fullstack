60e05543975c7b22f1417afcfd437bce28b5e66b
Marcin <mistamasta7@gmail.com>
2022-05-27 23:45:02 +0200

    Fix RWD & PWA

--- src/components/views/NotFound/NotFound.js
+++ src/components/views/NotFound/NotFound.js
@@ -1,13 +1,17 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
 import styles from './NotFound.module.scss';
 
-const Component = ({className, children}) => (
+const Component = ({className}) => (
   <div className={clsx(className, styles.root)}>
-    <h2>404 NotFound</h2>
-    {children}
+    <h1>404 Page not found</h1>
+    <img src="https://cdn.dribbble.com/users/1484654/screenshots/12124455/media/767417328eeb9e14c1cb40c69e323591.png?compress=1&resize=400x300" alt="no404"></img>
+    <div className={styles.button}>
+      <a href={`${process.env.PUBLIC_URL}/`} > Back to Homepage</a>
+    </div>
   </div>
 );
 
