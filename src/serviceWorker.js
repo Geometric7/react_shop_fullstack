0d698b2b234dbc3f1428ae3048daf4dca308e227
Marcin <mistamasta7@gmail.com>
2022-05-28 23:22:45 +0200

    Fix serviceWorker.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,5 @@
+
+
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -32,7 +34,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
